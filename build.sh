6e116b04e80c9bd6bf838a9c8ff003b6a117fa0a
Michael Baylis <Michael.Baylis@uk.ibm.com>
2021-05-26 21:58:07 +0100

    Add new build script

---
+++ build.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+set -e
+
+echo "Cleaning the openapi-go directory"
+
+rm -rf /workspace/openapi-go/*
+
+echo "Generate the OpenAPI Go client
+
+java -jar /opt/openapi/openapi-generator-cli.jar generate -g go --package-name runs.api.galasa.dev -i /workspace/openapi/runs/openapi.yaml -o /workspace/openapi-go
+
+echo "OpenAPI Go client has been created in /workspace/openapi-go"
