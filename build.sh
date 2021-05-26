4a69d78c3f0a385ec0ab0e421b0997c560140327
Michael Baylis <Michael.Baylis@uk.ibm.com>
2021-05-26 22:02:09 +0100

    syntax error

--- build.sh
+++ build.sh
@@ -6,7 +6,7 @@ echo "Cleaning the openapi-go directory"
 
 rm -rf /workspace/openapi-go/*
 
-echo "Generate the OpenAPI Go client
+echo "Generate the OpenAPI Go client"
 
 java -jar /opt/openapi/openapi-generator-cli.jar generate -g go --package-name runs.api.galasa.dev -i /workspace/openapi/runs/openapi.yaml -o /workspace/openapi-go
 
